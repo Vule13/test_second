ed8aa427ce45606bed3558629c6ef218f04ac6d3
Le <vulecr13@gmail.com>
2023-07-28 18:54:51 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(this).scrollTop()){
+      $('header').addClass('sticky');
+    }
+    else{
+      $('header').removeClass('sticky');
+    }
+  });
+});
+$(document).ready(function(){
+  $('.header-taskbar').click(function(){
+    $('ul').toggleClass('show')
+    $('.icon-btn').toggleClass('show')
+  });
+});
+// window.addEventListener("scroll", function(){
+//   var header = this.document.querySelector("header");
+//   header.classList.toggle("sticky", window.scrollY > 0)
+// })
